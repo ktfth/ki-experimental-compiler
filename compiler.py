718d4afefc98b56db998612fac9ff21097af4993
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-04-22 15:40:34 -0300

    Basic example of a calculator

--- compiler.py
+++ compiler.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# calclex.py
+#
+# tokenizer for a simple expression evaluator for
+# numbers and +,-,*,/
+# ------------------------------------------------------------
 import ply.lex as lex
 
 # List of token names.   This is always required
@@ -42,19 +48,55 @@ def t_error(t):
 # Build the lexer
 lexer = lex.lex()
 
-# Test it out
-data = '''
-3 + 4 * 10
-  + -20 *2
-'''
+import ply.yacc as yacc
 
-# Give the lexer some input
-lexer.input(data)
+# Get the token map from the lexer.  This is required.
+# from calclex import tokens
 
-# Tokenize
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    p[0] = p[1] + p[3]
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    p[0] = p[1] - p[3]
+
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    p[0] = p[1] * p[3]
+
+def p_term_div(p):
+    'term : term DIVIDE factor'
+    p[0] = p[1] / p[3]
+
+def p_term_factor(p):
+    'term : factor'
+    p[0] = p[1]
+
+def p_factor_num(p):
+    'factor : NUMBER'
+    p[0] = p[1]
+
+def p_factor_expr(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
+# Build the parser
+parser = yacc.yacc()
+
 while True:
-    tok = lexer.token()
-    if not tok:
-        break      # No more input
-    # print(tok)
-    print(tok.type, tok.value, tok.lineno, tok.lexpos)
+   try:
+       s = input('calc > ')
+   except EOFError:
+       break
+   if not s: continue
+   result = parser.parse(s)
+   print(result)
