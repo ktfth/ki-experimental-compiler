aa68307762f86470959fdb5856a808dbc1ff9d3c
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-04-22 19:44:33 -0300

    Working on assignment and accessment

--- compiler.py
+++ compiler.py
@@ -8,8 +8,12 @@
 import sys
 import ply.lex as lex
 
+reserved = {
+    'print': 'PRINT'
+}
+
 # List of token names.   This is always required
-tokens = (
+tokens = [
    'NUMBER',
    'PLUS',
    'MINUS',
@@ -17,9 +21,11 @@ tokens = (
    'DIVIDE',
    'LPAREN',
    'RPAREN',
-   'PRINT',
+   'ID',
    'SEMI'
-)
+] + list(reserved.values())
+
+literals = ['=']
 
 # Regular expression rules for simple tokens
 t_PLUS    = r'\+'
@@ -30,6 +36,11 @@ t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_PRINT = r'print'
 t_SEMI = r'\;'
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
 
 # A regular expression rule with some action code
 def t_NUMBER(t):
@@ -54,6 +65,20 @@ def t_error(t):
 lexer = lex.lex()
 
 import ply.yacc as yacc
+
+names = {}
+
+def p_statement_assign(p):
+    'expression : ID "=" expression'
+    names[p[1]] = p[3]
+
+def p_expression_name(p):
+    "expression : ID"
+    try:
+        p[0] = names[p[1]]
+    except LookupError:
+        print("Undefined name '%s'" % p[1])
+        p[0] = 0
 
 def p_expression_plus(p):
     'expression : expression PLUS term'
@@ -93,7 +118,10 @@ def p_statement_print(p):
 
 # Error rule for syntax errors
 def p_error(p):
-    print("Syntax error in input!")
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
 
 # Build the parser
 ki = yacc.yacc()
